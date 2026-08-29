e7e2f92309806f9b88748d8b7cff8d5d41d89644
Varsha <varshagaddam30@gmail.com>
2026-08-29 07:06:51 +0000

    fix: remediate 1 vulnerability(ies) in cmd/init.go
    Fixed vulnerabilities:
    - "switch" statements should have "default" clauses
      Add a default clause to this "switch" statement.

--- cmd/init.go
+++ cmd/init.go
@@ -962,12 +962,13 @@ func initHTTPServer(cfg *Config, urlCfg *UrlConfig, i *i18n.I18n, fs stuffbin.Fi
 		uploadFsURI    = ko.String("upload.filesystem.upload_uri")
 		publicURL      = ko.String("upload.s3.public_url")
 	)
-	switch {
-	case uploadProvider == "filesystem" && uploadFsURI != "":
-		srv.Static(uploadFsURI, ko.String("upload.filesystem.upload_path"))
-	case uploadProvider == "s3" && strings.HasPrefix(publicURL, "/"):
-		srv.GET(path.Join(publicURL, "/:filepath"), app.ServeS3Media)
-	}
+    switch {
+    case uploadProvider == "filesystem" && uploadFsURI != "":
+        srv.Static(uploadFsURI, ko.String("upload.filesystem.upload_path"))
+    case uploadProvider == "s3" && strings.HasPrefix(publicURL, "/"):
+        srv.GET(path.Join(publicURL, "/:filepath"), app.ServeS3Media)
+    default:
+    }
 
 	// Register all HTTP handlers.
 	initHTTPHandlers(srv, app)
